e4e909ef7f066d448efd34e2da956b7d343cbc6e
nahid na <absiddik718@gmail.com>
2024-02-04 17:24:19 +0600

    Changes settings

---
+++ client/app/loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loading = () => {
+    console.log("Loading ");
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full border-t-4 border-blue-500 border-opacity-25 h-16 w-16">
+        Please Wait
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
